c5e2d3caaa568556055391024f64319d01ab0504
Rafael Santis <rafael.santis@gmail.com>
2021-06-15 00:52:49 -0300

    Inclusão de login facebook

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const express         =     require('express')
   , session           =     require('express-session')
   , bodyParser        =     require('body-parser')
   , config            =     require('./configuration/config')
+  , facebook_config   =     require('./configuration/facebook_config')
   , app               =     express();
 
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.use(new GoogleStrategy({
     clientID: config.api_key,
@@ -16,6 +18,18 @@ passport.use(new GoogleStrategy({
   function(accessToken, refreshToken, profile, done) {
     return done(null, profile);
   }
+));
+
+passport.use(new FacebookStrategy({
+  clientID: facebook_config.api_key,
+  clientSecret: facebook_config.api_secret,
+  callbackURL: facebook_config.callback_url,
+
+},
+function(accessToken, refreshToken, profile, done) {
+  profile.picture = `https://graph.facebook.com/${profile.id}/picture?width=200&height=200&access_token=${accessToken}`
+  return done(null, profile);
+}
 ));
 
 // Passport session setup.
@@ -44,6 +58,15 @@ app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }))
 
 app.get('/auth/google/callback', 
   passport.authenticate('google',  { successRedirect : '/', failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook',  { successRedirect : '/', failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
   }
---
+++ configuration/facebook_config.js
@@ -0,0 +1,5 @@
+module.exports={
+    "api_key": "618974525730060",
+    "api_secret": "b9843227938e96c8a7ff161a18afc14e",
+    "callback_url": "http://localhost:3000/auth/facebook/callback"
+}
